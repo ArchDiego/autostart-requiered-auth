2784476a736499192f3e7de7afeb068aeef509ec
Diego Duran <96787803+ArchDiego@users.noreply.github.com>
2022-07-19 20:19:50 -0500

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,18 +1,3 @@
 #!/bin/sh
-
-sh /home/h4cknet/scripts/windows.sh &
-feh --bg-scale /home/h4cknet/Imágenes/wallpaper.png
-# systray battery icon
-cbatticon -u 5 &
-# systray volume
-volumeicon &
-pkill picom 
-picom --experimental-backends &
-run variety &
-run nm-applet &
-run pamac-tray &
-run xfce4-power-manager &
-numlockx on &
-blueberry-tray &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
